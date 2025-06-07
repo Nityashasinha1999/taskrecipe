267a77a1afa6d24fdeffdbd8735d22fa5b255c03
Nityasha <nityasha.s@reproindialtd.com>
2025-06-07 21:49:44 +0530

    Ui works: sort options need to be added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import TaskBoard from '@/components/TaskBoard'
 export default function Home() {
   return (
     <main className="min-h-screen p-8">
-      <div className="max-w-4xl mx-auto">
-        <h1 className="text-4xl font-bold mb-8">Task Manager</h1>
+      {/* <div className="max-w-4xl mx-auto">
+        <h1 className="text-4xl font-bold mb-8">Task Manager</h1> */}
         <TaskBoard />
-      </div>
+      {/* </div> */}
     </main>
   )
 }
